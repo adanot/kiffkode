fdf084e3c3dd21d6b8ec1d9f3d6a390791cec680
Adam Nottingham <adam@prism.fm>
2021-12-04 22:49:11 -0600

    simplify routing

--- src/AppWithRouterAccess.js
+++ src/AppWithRouterAccess.js
@@ -1,38 +1,11 @@
 import React from 'react';
-import { Route, useNavigate, Routes } from 'react-router-dom';
-import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
-import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
-import Home from './Home';
-import Login from './Login';
-import Protected from './Protected';
-import { oktaAuthConfig, oktaSignInConfig } from './config';
+import { BrowserRouter as Router, Route, useHistory } from 'react-router-dom';
 
-const oktaAuth = new OktaAuth(oktaAuthConfig);
 
-const AppWithRouterAccess = () => {
-  const history = useNavigate();
+const AppWithRouterAccess = () => (
+  <Router>
+    <App />
+  </Router>
+);
 
-  const customAuthHandler = () => {
-    history.push('/login');
-  };
-
-  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
-    history.replace(toRelativeUrl(originalUri, window.location.origin));
-  };
-
-  return (
-    <Security
-      oktaAuth={oktaAuth}
-      onAuthRequired={customAuthHandler}
-      restoreOriginalUri={restoreOriginalUri}
-    >
-      <Routes>
-        <Route path='/' exact={true} component={Home} />
-        <SecureRoute path='/protected' component={Protected} />
-        <Route path='/login' render={() => <Login config={oktaSignInConfig} />} />
-        <Route path='/login/callback' component={LoginCallback} />
-      </Routes>
-    </Security>
-  );
-};
-export default AppWithRouterAccess;
+export default AppWithRouterAccesss;
